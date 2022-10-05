5b278ff60057758f3d94b8cfcb80abe909117728
farid <nzhang2@academic.rrc.ca>
2022-10-05 15:38:50 -0500

    Add About page via creating static_pages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root to: "static_pages#about"
+  get "/about", to: "static_pages#about", as: "about"
   resources :schools
   resources :home_addresses
   resources :school_division_wards
   resources :wards
   resources :school_divisions
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
