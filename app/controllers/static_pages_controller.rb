9b813431b8d60dd805efac5f7450bfe960f6219c
farid <nzhang2@academic.rrc.ca>
2022-10-08 22:27:55 -0500

    Create home page.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,29 @@
+NUM = 50
+
 class StaticPagesController < ApplicationController
-  def about
+  def about; end
+
+  def home
+    @num = NUM
+
+    @wards_count = Ward.count
+    @has_too_many_wards = @wards_count > NUM
+    @wards = @has_too_many_wards ? Ward.limit(NUM) : Ward.all
+
+    @school_divisions_count = SchoolDivision.count
+    @has_too_many_school_divisions = @school_divisions_count > NUM
+    @school_divisions = @has_too_many_school_divisions ? SchoolDivision.limit(NUM) : SchoolDivision.all
+
+    @school_division_wards_count = SchoolDivisionWard.count
+    @has_too_many_school_division_wards = @school_division_wards_count > NUM
+    @school_division_wards = @has_too_many_school_division_wards ? SchoolDivisionWard.limit(NUM) : SchoolDivisionWard.all
+
+    @schools_count = School.count
+    @has_too_many_schools = @schools_count > NUM
+    @schools = @has_too_many_schools ? School.limit(NUM) : School.all
+
+    @home_addresses_count = HomeAddress.count
+    @has_too_many_home_addresses = @home_addresses_count > NUM
+    @home_addresses = @has_too_many_home_addresses ? HomeAddress.limit(NUM) : HomeAddress.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "schools#index"
+  root to: "static_pages#home"
   get "/about", to: "static_pages#about", as: "about"
   resources :schools
   resources :home_addresses
